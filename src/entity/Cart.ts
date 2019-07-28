953d2a01dd43a9eb65312cc7e1cbdfd72d9d15e5
yassineaachir123 <yassineaachir123@gmail.com>
2019-07-28 20:51:20 +0100

    Add Cart Entity

---
+++ src/entity/Cart.ts
@@ -0,0 +1,25 @@
+import {Entity, PrimaryGeneratedColumn, ManyToOne, Column, BaseEntity, ManyToMany, JoinTable} from "typeorm";
+import { ObjectType, Field, ID } from "type-graphql";
+import {Product} from "./Product";
+import {Coupon} from "./Coupon";
+
+@ObjectType()
+@Entity()
+export class Cart extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Field()
+  @Column()
+  public Quantity: number;
+
+  @ManyToMany(() => Product)
+  @JoinTable()
+  public product: Product[];
+
+  @ManyToOne(() => Coupon, (coupon: Coupon) => coupon.carts)
+  public coupon: Coupon;
+
+
+}
