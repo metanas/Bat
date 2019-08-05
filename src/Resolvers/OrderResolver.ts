b9f97f44656bd8bd4f988deb05e81271728bc86e
yassineaachir123 <yassineaachir123@gmail.com>
2019-08-05 21:03:13 +0100

    fix OrderResolver

--- src/Resolvers/OrderResolver.ts
+++ src/Resolvers/OrderResolver.ts
@@ -5,6 +5,13 @@ import {Order} from "../entity/Order";
 import {User} from "../entity/User";
 import {Coupon} from "../entity/Coupon";
 import {getConnection} from "typeorm";
+import {PaginatedResponseInput} from "../Modules/inputs/PaginatedResponseInput";
+import PaginatedResponse from "../Modules/interfaces/PaginatedResponse";
+import { ceil } from "lodash";
+
+const PaginatedOrderResponse = PaginatedResponse(Order);
+// @ts-ignore
+type PaginatedOrderResponse = InstanceType<typeof PaginatedOrderResponse>;
 
 @Resolver()
 export class OrderResolver {
@@ -31,10 +38,33 @@ export class OrderResolver {
     }
 
   @UseMiddleware(Auth)
+  @Mutation(() => Order)
+  public async UpdateOrderStatus(@Arg("id") id: number, @Arg("status") status: string){
+    return await getConnection()
+      .createQueryBuilder()
+      .update(Order)
+      .set({status})
+      .where("id=:id", {id})
+      .execute();
+  }
+
+  @UseMiddleware(Auth)
   @Mutation(() => Boolean)
   public async deleteOrder(@Arg("id") id: number) {
     const order = getConnection().createQueryBuilder().delete().from(Order)
       .where("id=:id", {id}).execute();
     return !!order
+  }
+
+  @UseMiddleware(Auth)
+  @Query(() => PaginatedOrderResponse)
+  public async getOrders(@Ctx() ctx: ApiContext, @Arg("data") { page, limit }: PaginatedResponseInput ): Promise<PaginatedOrderResponse> {
+    const user = await User.findOne({ where: { id: ctx.req.session!.token }});
+    const result = await Order.findAndCount({where: {user}, skip: page - 1, take: limit});
+    return {
+      items: result[0],
+      totalPages: ceil(result[1] / limit),
+      totalCount: result[1]
+    };
   }
 }
