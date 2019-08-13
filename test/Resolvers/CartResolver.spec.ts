acc475a0685db83ec4499e9f3e7b9787468fb313
yassineaachir123 <yassineaachir123@gmail.com>
2019-08-13 22:30:24 +0100

    test CartResolver

---
+++ test/Resolvers/CartResolver.spec.ts
@@ -0,0 +1,60 @@
+import {User} from "../../src/entity/User";
+import {Connection} from "typeorm";
+import {connection} from "../test-utils/connection";
+import faker = require("faker");
+import {Cart} from "../../src/entity/Cart";
+import {graphqlCall} from "../test-utils/graphqlCall";
+
+
+
+let conn: Connection;
+let user: User;
+
+beforeAll(async () => {
+  conn = await connection();
+});
+
+afterAll(async () => {
+  await conn.close();
+});
+
+async function createUser() {
+  user = await User.create({
+    name: faker.name.firstName() + " " + faker.name.lastName(),
+    telephone: faker.phone.phoneFormats(),
+    birthday: faker.date.past().toDateString()
+  }).save();
+}
+
+describe("Test Cart Resolver",  () => {
+  it("Test Getting Carte By UserID", async () => {
+    await createUser();
+
+    const cart = await Cart.create({
+      user: user
+    }).save();
+
+    const getCartQuery = `{
+    getCart(id: ${cart.id}){
+      id
+    }
+    }`;
+
+    const response = await graphqlCall({
+      source: getCartQuery,
+      token: user.id
+
+    });
+
+    expect(response).toMatchObject({
+      data:{
+        getCart : {
+          id: `${cart.id}`,
+        }
+      }
+    });
+  });
+
+
+
+});
