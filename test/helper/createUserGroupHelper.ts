3b7d4973d812465ad4b1fd9aff31d049386478e5
metanas <com.com404@gmail.com>
2019-09-03 22:55:50 +0100

    Add Permission to Admin Resolvers

--- test/helper/createUserGroupHelper.ts
+++ test/helper/createUserGroupHelper.ts
@@ -4,6 +4,6 @@ import faker from "faker";
 export async function createUserGroupHelper() {
   return await UserGroup.create({
     name: faker.name.jobTitle(),
-    permissions: JSON.parse("{ \"access\": [\"Test\", \"test2\"], \"modify\": [\"Test3\", \"test4\"] }")
+    permissions: JSON.parse("{ \"access\": [\"AddProduct\", \"test2\"], \"modify\": [\"Test3\", \"test4\"] }")
   }).save()
 }
